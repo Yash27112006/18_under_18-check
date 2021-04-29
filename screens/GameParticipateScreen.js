0d35ec0cc6061d479f1f11d0923310f971b06b97
Yash27112006 <64268919+Yash27112006@users.noreply.github.com>
2021-04-29 22:09:16 +0530

    Add files via upload

---
+++ screens/GameParticipateScreen.js
@@ -0,0 +1,121 @@
+//working
+
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { ListItem } from "react-native-elements";
+import firebase from "firebase";
+import db from "../config";
+import MyHeader from "../components/MyHeader";
+
+export default class GameParticipateScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userId: firebase.auth().currentUser.email,
+      requestedPlayersList: [],
+    };
+    this.requestRef = null;
+  }
+
+  getRequestedPlayerssList = () => {
+    this.requestRef = db
+      .collection("requested_games")
+      .onSnapshot((snapshot) => {
+        var requestedPlayersList = snapshot.docs.map((doc) => doc.data());
+        this.setState({
+          requestedPlayersList: requestedPlayersList,
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.getRequestedPlayerssList();
+  }
+
+  componentWillUnmount() {
+  this.requestRef();
+  }
+
+  keyExtractor = (item, index) => index.toString();
+
+  renderItem = ({ item, i }) => {
+    return (
+      <ListItem
+        key={i}
+        title={item.game_name}
+        subtitle={<Text>Players: {item.players_i_have}</Text>}
+        titleStyle={{ color: "black", fontWeight: "bold" }}
+        leftElement={
+          <Text>Play At: {item.play_time}</Text>
+         }
+        
+        rightElement={
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              this.props.navigation.navigate("PlayerDetails", {
+                details: item,
+              });
+            }}
+          >
+            <Text style={{ color: "#ffff" }}>View</Text>
+          </TouchableOpacity>
+        }
+        bottomDivider
+      />
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.view}>
+        <MyHeader title="Participate Games" navigation={this.props.navigation} />
+        <View style={{ flex: 1 }}>
+          {this.state.requestedPlayersList.length === 0 ? (
+            <View style={styles.subContainer}>
+              <Text style={{ fontSize: 20 }}>List Of All Requested Games</Text>
+            </View>
+          ) : (
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={this.state.requestedPlayersList}
+              renderItem={this.renderItem}
+            />
+          )}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  subContainer: {
+    flex: 1,
+    fontSize: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    width: 100,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#32867d",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+  },
+  view:{
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
